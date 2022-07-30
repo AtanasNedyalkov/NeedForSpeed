ffb49bec5092bf2716cad53c19b492890401d005
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-07-30 14:39:49 +0300

    Add project files.

---
+++ RaceMotorcycle.cs
@@ -0,0 +1,16 @@
+﻿
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double RaceMotorcycleDefaultFuelConsumation = 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+        public override double FuelConsumption
+        {
+            get { return RaceMotorcycleDefaultFuelConsumation; }
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+        public int HorsePower { get; set; }
+        public double  Fuel { get; set; }
+        public const double DefaultFuelConsumption = 1.25;
+        public virtual double FuelConsumption
+        { 
+            get
+            {
+                return DefaultFuelConsumption;
+            } 
+           
+        }
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -=  FuelConsumption * kilometers;
+        }
+    }
+}
